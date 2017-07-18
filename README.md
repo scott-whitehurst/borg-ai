22e3b6e8d91bcf4951e00855b752fa31ac9e4649
Scott Whitehurst <scott.whitehurst@qa.com>
2017-07-18 11:01:19 +0000

    Added username to responses

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # borg-ai
+
+Functionality:
+
+Echo the user's name when invoked
+
+When time is given as a parameter, provide two strings referring to the time of day and whether the user should be on/off the job
--- pixal.sh
+++ pixal.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 case "$1" in
+'')
+echo "Hello $USER, please enter a command"
+esac
+
+case "$1" in
 'time')
 
 HOUR=$(date "+%H")
@@ -9,15 +14,15 @@ TIME=$(date "+%T")
 
 if [ "$HOUR" -le "11" ]
 then
-  echo "Good Morning, the time is now $TIME"
+  echo "Good Morning $USER, the time is now $TIME"
 elif [ "$HOUR" -ge "13" ] && [ "$HOUR" -le "16" ]
 then
-  echo "Good Afternoon, it is precisely $TIME"
+  echo "Good Afternoon $USER, it is precisely $TIME"
 elif [ "$HOUR" -ge "17" ] && [ "$HOUR" -le "20" ]
 then
-  echo "Good Evening, the time is currently $TIME"
+  echo "Good Evening $USER, the time is currently $TIME"
 else
-  echo "You should really be in bed"
+  echo "You should really be in bed, $USER"
 fi
 
 if [ "$TODAY" -eq "6" ] || [ "$TODAY" -eq "7" ] 
