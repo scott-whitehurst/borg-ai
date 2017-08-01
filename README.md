ef36c99e9039fa9119296d436a495628b5ea2427
Scott Whitehurst <scott.whitehurst@qa.com>
2017-08-01 13:20:01 +0000

    Amended case statement structure to proper

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Functionality based on input:
 
 None:
-Echo the user's name when invoked
+Echo the user's name when invoked, list the available functions
 
 Time:
 When time is given as a parameter, provide two strings referring to the time of day and whether the user should be on/off the job
@@ -12,7 +12,7 @@ Dice:
 Very basic diceroll to echo number from 1 to 6
 
 Recycle:
-Based on user input, move a file to the trash directory - consider configuring a cron job to empty this at the start of every month
+Based on user input, move a file to the trash directory (reporting error if file not found) - consider configuring a cron job to empty this at the start of every month
 
 Game:
 Play a game of rock, paper, scissors with the user - there is probably a simpler way to code this, but for now it seems to give fair odds
--- pixal.sh
+++ pixal.sh
@@ -3,14 +3,12 @@
 case "$1" in
 '')
 echo "Hello $USER, please enter a command, or invoke 'help' for a list of actions"
-esac
+;;
 
-case "$1" in
 'help')
 echo "What can I help you with, $USER? Please choose from: 'time' 'dice' 'recycle' 'game'"
-esac
+;;
 
-case "$1" in
 'time')
 
 HOUR=$(date "+%H")
@@ -37,26 +35,26 @@ else
   echo "You should currently be working"
 fi
 ;;
-esac
 
-case "$1" in
 'dice')
 
 DICE=$(echo $RANDOM % 6 + 1 | bc)
 
 echo "You have rolled the number $DICE"
-esac
+;;
 
-case "$1" in
 'recycle')
 
 echo -n "Hello $USER, please provide the full path to the file you wish to recycle and press [ENTER]: "
 read junk
 mv $junk ~/trash/
+if [ $? == "1" ]
+then echo "Sorry I have been unable to locate that file, please check the directory path"
+else
 echo "Your unwanted files have been placed in the recycling bin, this will be emptied at the start of next month"
-esac
+fi
+;;
 
-case "$1" in
 'game')
 
 PIXTHROW=$(echo $RANDOM % 99 + 1 | bc)
@@ -65,23 +63,23 @@ echo -n "Okay $USER the game is Rock, Paper, Scissors. Make your choice and hit 
 read throw
 echo "You threw $throw"
 if [ $throw == "Rock" ] && [ "$PIXTHROW" -le "33" ]
-then echo -en "I throw rock\nIt is a draw"
+then echo -e "I throw rock\nIt is a draw"
 elif [ $throw == "Rock" ] && [ "$PIXTHROW" -ge "34" -a "$PIXTHROW" -le "66" ]
-then echo -en "I throw paper\nI have won ^^"
+then echo -e "I throw paper\nI have won ^^"
 elif [ $throw == "Rock" ] && [ "$PIXTHROW" -ge "67" -a "$PIXTHROW" -le "99" ]
-then echo -en "I throw scissors\nYou have won, well done $USER"
+then echo -e "I throw scissors\nYou have won, well done $USER"
 elif [ $throw == "Paper" ] && [ "$PIXTHROW" -le "33" ]
-then echo -en "I throw rock\nYou have beaten me!"
+then echo -e "I throw rock\nYou have beaten me!"
 elif [ $throw == "Paper" ] && [ "$PIXTHROW" -ge "34" -a "$PIXTHROW" -le "66" ]
-then echo -en "I throw Paper\nAh, it is a draw"
+then echo -e "I throw Paper\nAh, it is a draw"
 elif [ $throw == "Paper" ] && [ "$PIXTHROW" -ge "67" -a "$PIXTHROW" -le "99" ]
-then echo -en "I throw scissors\nI have triumphed over $USER"
+then echo -e "I throw scissors\nI have triumphed over $USER"
 elif [ $throw == "Scissors" ] && [ "$PIXTHROW" -le "33" ]
-then echo -en "I throw rock\nMy rock beats your scissors!"
+then echo -e "I throw rock\nMy rock beats your scissors!"
 elif [ $throw == "Scissors" ] && [ "$PIXTHROW" -ge "34" -a "$PIXTHROW" -le "66" ]
-then echo -en "I throw paper\nCongratulations, $USER"
+then echo -e "I throw paper\nCongratulations, $USER"
 elif [ $throw == "Scissors" ] && [ "$PIXTHROW" -ge "67" -a "$PIXTHROW" -le "99" ]
-then echo -en "I throw scissors\nTwo scissors? It is a draw then"
+then echo -e "I throw scissors\nTwo scissors? It is a draw then"
 else echo "Sorry, I didn't register that, please try again"
 fi
 ;;
